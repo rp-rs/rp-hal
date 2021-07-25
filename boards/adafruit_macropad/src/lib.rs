25cf81fdfea5276896eab5d60bad1d65e2120379
Andrea Nall <anall@andreanall.com>
2021-07-25 16:56:30 -0500

    Add Adafruit Macropad BSP

---
+++ boards/adafruit_macropad/src/lib.rs
@@ -0,0 +1,64 @@
+#![no_std]
+
+pub use rp2040_hal as hal;
+
+#[cfg(feature = "rt")]
+extern crate cortex_m_rt;
+#[cfg(feature = "rt")]
+pub use cortex_m_rt::entry;
+
+pub use hal::pac;
+
+hal::bsp_pins!(
+    Gpio0 { name: button },
+    Gpio1 { name: key1 },
+    Gpio2 { name: key2 },
+    Gpio3 { name: key3 },
+    Gpio4 { name: key4 },
+    Gpio5 { name: key5 },
+    Gpio6 { name: key6 },
+    Gpio7 { name: key7 },
+    Gpio8 { name: key8 },
+    Gpio9 { name: key9 },
+    Gpio10 { name: key10 },
+    Gpio11 { name: key11 },
+    Gpio12 { name: key12 },
+    Gpio13 { name: led },
+    Gpio14 {
+        name: speaker_shutdown
+    },
+    Gpio15 { name: speaker },
+    Gpio17 {
+        name: encoder_rota,
+        aliases: { PullUpInput: RotaryA }
+    },
+    Gpio18 {
+        name: encoder_rotb,
+        aliases: { PullUpInput: RotaryB }
+    },
+    Gpio19 { name: neopixel },
+    Gpio20 {
+        name: sda,
+        aliases: { FunctionI2C: Sda }
+    },
+    Gpio21 {
+        name: scl,
+        aliases: { FunctionI2C: Scl }
+    },
+    // This CS is on the wrong SPI channel so cannot be controlled by the peripheral
+    Gpio22 { name: oled_cs },
+    Gpio23 { name: oled_reset },
+    Gpio24 { name: oled_dc },
+    Gpio26 {
+        name: sclk,
+        aliases: { FunctionSpi: Sclk }
+    },
+    Gpio27 {
+        name: mosi,
+        aliases: { FunctionSpi: Mosi }
+    },
+    Gpio28 {
+        name: miso,
+        aliases: { FunctionSpi: Miso }
+    },
+);
