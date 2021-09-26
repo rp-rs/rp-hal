d93c4fc4c864329d936aff321ce1b419092e744b
Rob Young <bubblenut@gmail.com>
2021-09-26 11:51:01 +0100

    Implement RngCore for RingOscillator (#135)
    Implement rand_core::RngCore for RingOscillator from the get_random_bit
    function. This is not suitable for security purposes so
    rand_core::CryptoRng has not been implemented.

--- rp2040-hal/src/rosc.rs
+++ rp2040-hal/src/rosc.rs
@@ -97,3 +97,28 @@ impl RingOscillator<Enabled> {
         self.transition(Dormant)
     }
 }
+
+impl rand_core::RngCore for RingOscillator<Enabled> {
+    fn next_u32(&mut self) -> u32 {
+        rand_core::impls::next_u32_via_fill(self)
+    }
+
+    fn next_u64(&mut self) -> u64 {
+        rand_core::impls::next_u64_via_fill(self)
+    }
+
+    fn fill_bytes(&mut self, dest: &mut [u8]) {
+        for chunk in dest.iter_mut() {
+            *chunk = 0_u8;
+            for _ in 0..8 {
+                *chunk <<= 1;
+                *chunk ^= self.get_random_bit() as u8;
+            }
+        }
+    }
+
+    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), rand_core::Error> {
+        self.fill_bytes(dest);
+        Ok(())
+    }
+}
