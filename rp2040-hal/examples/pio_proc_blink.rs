6a7bf39bd37315b9f3b6eded018fafb127d33fb4
9names <60134748+9names@users.noreply.github.com>
2021-10-03 23:13:36 +1100

    Update HAL to use same boot2 as BSPs, and hal/examples to use W25Q080

--- rp2040-hal/examples/pio_proc_blink.rs
+++ rp2040-hal/examples/pio_proc_blink.rs
@@ -14,7 +14,7 @@ use rp2040_hal as hal;
 
 #[link_section = ".boot2"]
 #[used]
-pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER;
+pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 
 #[entry]
 fn main() -> ! {
