37eb85362d149b699ba4376177ba19ffe528ebc0
Jan Niehusmann <jan@gondor.com>
2021-10-17 15:45:33 +0000

    Improve doc comments

--- rp2040-hal/src/atomic_register_access.rs
+++ rp2040-hal/src/atomic_register_access.rs
@@ -1,40 +1,56 @@
-//! Provide atomic access to registers, as described in
-//! [section 2.1.2 of the reference manual](https://datasheets.raspberrypi.com/rp2040/rp2040-datasheet.pdf#atomic-rwtype)
+//! Provide atomic access to peripheral registers
+//!
+//! This feature is not available for all peripherals.
+//! See [section 2.1.2 of the RP2040 datasheet][section_2_1_2] for details.
+//!
+//! [section_2_1_2]: https://datasheets.raspberrypi.com/rp2040/rp2040-datasheet.pdf#atomic-rwtype
 
 use core::ptr::write_volatile;
 
 /// Perform atomic XOR operation on register
 ///
+/// See [section 2.1.2 of the RP2040 datasheet][section_2_1_2] for details.
+///
+/// [section_2_1_2]: https://datasheets.raspberrypi.com/rp2040/rp2040-datasheet.pdf#atomic-rwtype
+///
 /// # Safety
 ///
 /// In addition to the requirements of [core::ptr::write_volatile],
-/// reg must point to a register providing atomic aliases.
+/// `register` must point to a register providing atomic aliases.
 #[inline]
-pub unsafe fn write_xor(reg: *mut u32, bits: u32) {
-    let alias = (reg as usize + 0x1000) as *mut u32;
+pub unsafe fn write_xor(register: *mut u32, bits: u32) {
+    let alias = (register as usize + 0x1000) as *mut u32;
     write_volatile(alias, bits);
 }
 
 /// Perform atomic bitmask set operation on register
 ///
+/// See [section 2.1.2 of the RP2040 datasheet][section_2_1_2] for details.
+///
+/// [section_2_1_2]: https://datasheets.raspberrypi.com/rp2040/rp2040-datasheet.pdf#atomic-rwtype
+///
 /// # Safety
 ///
 /// In addition to the requirements of [core::ptr::write_volatile],
-/// reg must point to a register providing atomic aliases.
+/// `register` must point to a register providing atomic aliases.
 #[inline]
-pub unsafe fn write_bitmask_set(reg: *mut u32, bits: u32) {
-    let alias = (reg as usize + 0x2000) as *mut u32;
+pub unsafe fn write_bitmask_set(register: *mut u32, bits: u32) {
+    let alias = (register as usize + 0x2000) as *mut u32;
     write_volatile(alias, bits);
 }
 
 /// Perform atomic bitmask clear operation on register
 ///
+/// See [section 2.1.2 of the RP2040 datasheet][section_2_1_2] for details.
+///
+/// [section_2_1_2]: https://datasheets.raspberrypi.com/rp2040/rp2040-datasheet.pdf#atomic-rwtype
+///
 /// # Safety
 ///
 /// In addition to the requirements of [core::ptr::write_volatile],
-/// reg must point to a register providing atomic aliases.
+/// `register` must point to a register providing atomic aliases.
 #[inline]
-pub unsafe fn write_bitmask_clear(reg: *mut u32, bits: u32) {
-    let alias = (reg as usize + 0x3000) as *mut u32;
+pub unsafe fn write_bitmask_clear(register: *mut u32, bits: u32) {
+    let alias = (register as usize + 0x3000) as *mut u32;
     write_volatile(alias, bits);
 }
