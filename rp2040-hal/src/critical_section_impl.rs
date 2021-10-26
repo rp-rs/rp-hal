df8d98b439d025058d03fcc63f6cd478d08384d3
9names <60134748+9names@users.noreply.github.com>
2021-10-26 20:15:31 +1100

    Get rid of count variable, switch to AtomicU8 for lock state

--- rp2040-hal/src/critical_section_impl.rs
+++ rp2040-hal/src/critical_section_impl.rs
@@ -1,24 +1,27 @@
+use core::sync::atomic::{AtomicU8, Ordering};
+
 struct RpSpinlockCs;
 critical_section::custom_impl!(RpSpinlockCs);
 
-const LOCK_CORE_SENTINEL: u32 = 0xDEADBEEF;
-// These may or may not be shared between cores
-// If shared, we need to access them with interrupts disabled.
-// TODO: move away from static mut to avoid potential static init issues.
-static mut LOCK_CORE: u32 = LOCK_CORE_SENTINEL;
-static mut LOCK_COUNT: u8 = 0;
+// Value to indicate no-one has the lock.
+// Initialising LOCK_CORE to 0 means cheaper initialisation.
+const LOCK_CORE_SENTINEL: u8 = 0;
+static mut LOCK_CORE: AtomicU8 = AtomicU8::new(LOCK_CORE_SENTINEL);
+// Value to indicate that we're not the inner loop
+// Only needs to be not 0 or 1, but might as well make it unique
+const INNER_LOOP_VALUE: u8 = 0xee;
 
 unsafe impl critical_section::Impl for RpSpinlockCs {
     unsafe fn acquire() -> u8 {
         // Store the initial interrupt state and current core id in stack variables
         let interrupts_active = cortex_m::register::primask::read().is_active();
-        let core = (*pac::SIO::ptr()).cpuid.read().bits();
+        let core = (*pac::SIO::ptr()).cpuid.read().bits() as u8 + 1_u8;
         // Need interrupts to be disabled to check/set our static variables
         cortex_m::interrupt::disable();
-        // Safety: LOCK_COUNT won't change if LOCK_CORE = current core
-        if LOCK_CORE == core && LOCK_COUNT != 0 {
+        if LOCK_CORE.load(Ordering::Acquire) == core {
             // We already own the lock, so we must have called acquire within a critical_section.
-            // Don't need to do anything here.
+            // Return the magic inner-loop value so that we know not to re-enable interrupts in release()
+            INNER_LOOP_VALUE
         } else {
             // Spin until we get the lock
             loop {
@@ -29,7 +32,7 @@ unsafe impl critical_section::Impl for RpSpinlockCs {
                 if (*pac::SIO::ptr()).spinlock31.read().bits() != 0 {
                     // We just acquired the lock.
                     // Store which core we are so we can tell if we're called recursively
-                    LOCK_CORE = core;
+                    LOCK_CORE.store(core, Ordering::Release);
                     break;
                 }
                 // We didn't get the lock, enable interrupts if they were enabled before we started
@@ -37,20 +40,18 @@ unsafe impl critical_section::Impl for RpSpinlockCs {
                     cortex_m::interrupt::enable();
                 }
             }
-            // If we broke out of the loop we have just acquired the lock.
+            // If we broke out of the loop we have just acquired the lock
+            // As the outermost loop, we want to return the interrupt status to restore later
+            interrupts_active as _
         }
-        LOCK_COUNT += 1;
-        interrupts_active as _
     }
 
     unsafe fn release(token: u8) {
-        // Decrement our lock count
-        LOCK_COUNT -= 1;
         // Was this the outermost critical_section?
-        if LOCK_COUNT == 0 {
-            // Yes, we were the last.
+        if token != INNER_LOOP_VALUE {
+            // Yes, we were the outermost.
             // Set LOCK_CORE to the sentinel value to ensure the next call checks spinlock instead
-            LOCK_CORE = LOCK_CORE_SENTINEL;
+            LOCK_CORE.store(LOCK_CORE_SENTINEL, Ordering::Release);
             // Release our spinlock
             (*pac::SIO::ptr()).spinlock31.write_with_zero(|w| w.bits(1));
             // Re-enable interrupts if they were enabled when we first called acquire()
