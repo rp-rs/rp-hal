1ec8a4ab9227f2fc4773f058c46d46af148fdefb
Andrew Straw <strawman@astraw.com>
2022-02-01 12:33:57 +0100

    add example of synchronized PIOs

---
+++ rp2040-hal/examples/pio_synchronized.rs
@@ -0,0 +1,79 @@
+//! This example toggles the GPIO0 and GPIO1 pins, with each controlled from a
+//! separate PIO state machine.
+//!
+//! Despite running in separate state machines, the clocks are sychronized at
+//! the rise and fall times will be simultaneous.
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use hal::gpio::{FunctionPio0, Pin};
+use hal::pac;
+use hal::pio::PIOExt;
+use hal::Sio;
+use panic_halt as _;
+use rp2040_hal as hal;
+
+#[link_section = ".boot2"]
+#[used]
+pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
+
+#[entry]
+fn main() -> ! {
+    let mut pac = pac::Peripherals::take().unwrap();
+
+    let sio = Sio::new(pac.SIO);
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    // configure pins for Pio0.
+    let _: Pin<_, FunctionPio0> = pins.gpio0.into_mode();
+    let _: Pin<_, FunctionPio0> = pins.gpio1.into_mode();
+
+    // PIN id for use inside of PIO
+    let pin0 = 0;
+    let pin1 = 1;
+
+    // Define some simple PIO program.
+    let program = pio_proc::pio!(
+        32,
+        "
+.wrap_target
+    set pins, 1 [31]
+    set pins, 0 [31]
+.wrap
+        "
+    );
+
+    // Initialize and start PIO
+    let (mut pio, sm0, sm1, _, _) = pac.PIO0.split(&mut pac.RESETS);
+    let div = 0f32; // as slow as possible (0 is interpreted as 65536)
+
+    let installed = pio.install(&program.program).unwrap();
+    let (mut sm0, _, _) = rp2040_hal::pio::PIOBuilder::from_program(installed)
+        .set_pins(pin0, 1)
+        .clock_divisor(div)
+        .build(sm0);
+    // The GPIO pin needs to be configured as an output.
+    sm0.set_pindirs([(pin0, hal::pio::PinDir::Output)]);
+
+    // NOTE: with the current rp-hal, I need to call pio.install() twice. This
+    // should be investigated further as it seems wrong.
+    let installed = pio.install(&program.program).unwrap();
+    let (mut sm1, _, _) = rp2040_hal::pio::PIOBuilder::from_program(installed)
+        .set_pins(pin1, 1)
+        .clock_divisor(div)
+        .build(sm1);
+    // The GPIO pin needs to be configured as an output.
+    sm1.set_pindirs([(pin1, hal::pio::PinDir::Output)]);
+
+    sm0.synchronize_with(&mut sm1).start();
+
+    // PIO runs in background, independently from CPU
+    #[allow(clippy::empty_loop)]
+    loop {}
+}
