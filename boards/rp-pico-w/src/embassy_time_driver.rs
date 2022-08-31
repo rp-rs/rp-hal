a524d33717f61a298378189d08cdbcddfac66eaa
Jan Niehusmann <jan@gondor.com>
2022-08-31 20:39:37 +0000

    Uset timer::Instant

--- boards/rp-pico-w/src/embassy_time_driver.rs
+++ boards/rp-pico-w/src/embassy_time_driver.rs
@@ -16,20 +16,23 @@ use rp2040_hal::timer::Alarm;
 use rp2040_hal::timer::Alarm0;
 use rp2040_hal::timer::Timer;
 use rp2040_hal::timer::ScheduleAlarmError;
+use rp2040_hal::timer::Instant;
 
 use defmt::*;
 
 struct AlarmState {
-    timestamp: Cell<u64>,
+    timestamp: Cell<Instant>,
     #[allow(clippy::type_complexity)]
     callback: Cell<Option<(fn(*mut ()), *mut ())>>,
 }
 unsafe impl Send for AlarmState {}
 
 const ALARM_COUNT: usize = 4;
+const NO_ALARM: Instant = Instant::from_ticks(u64::MAX);
+
 #[allow(clippy::declare_interior_mutable_const)]
 const DUMMY_ALARM: AlarmState = AlarmState {
-    timestamp: Cell::new(0),
+    timestamp: Cell::new(NO_ALARM),
     callback: Cell::new(None),
 };
 
@@ -49,14 +52,7 @@ embassy_time::time_driver_impl!(static DRIVER: TimerDriver = TimerDriver{
 
 impl Driver for TimerDriver {
     fn now(&self) -> u64 {
-        critical_section::with(|cs| {
-            self.timer
-                .borrow(cs)
-                .borrow()
-                .as_ref()
-                .unwrap()
-                .get_counter()
-        })
+        self.now_instant().ticks()
     }
 
     unsafe fn allocate_alarm(&self) -> Option<AlarmHandle> {
@@ -87,23 +83,38 @@ impl Driver for TimerDriver {
 
     fn set_alarm(&self, alarm: AlarmHandle, timestamp: u64) {
         let n = alarm.id() as usize;
-        trace!("set alarm {} to {}", n, timestamp);
         critical_section::with(|cs| {
             let alarms = self.alarms.borrow(cs);
             let alarm = &alarms[n];
-            //let timer = self.timer.borrow(cs).borrow_mut().as_mut();
-            alarm.timestamp.set(timestamp);
+            if alarm.timestamp.get().ticks() == timestamp {
+                // redundant set_alarm - ignore
+                return;
+            }
+            alarm.timestamp.set(Instant::from_ticks(timestamp));
 
             self.arm(cs);
+            trace!("set alarm {} to {}", n, timestamp);
         })
     }
 }
 
 impl TimerDriver {
+    fn now_instant(&self) -> Instant {
+        critical_section::with(|cs| {
+            self.timer
+                .borrow(cs)
+                .borrow()
+                .as_ref()
+                .unwrap()
+                .get_counter()
+        })
+    }
+
     fn arm(&self, cs: CriticalSection) {
         let (n, min_timestamp) = self.next_scheduled_alarm();
-        if min_timestamp == u64::MAX {
+        if min_timestamp == NO_ALARM {
             // no alarm set
+            trace!("Trying to arm non-existing alarm");
             return;
         }
         // Arm it.
@@ -111,41 +122,43 @@ impl TimerDriver {
         // Note that we're not checking the high bits at all. This means the irq may fire early
         // if the alarm is more than 72 minutes (2^32 us) in the future. This is OK, since on irq fire
         // it is checked if the alarm time has passed.
-        let now = self.now();
-        if min_timestamp > now {
-            use fugit::MicrosDurationU32;
-            if self
-                .alarm
-                .borrow(cs)
-                .borrow_mut()
-                .deref_mut()
-                .as_mut()
-                .unwrap()
-                .schedule(MicrosDurationU32::micros((min_timestamp - now) as u32))
-                == core::result::Result::Err(ScheduleAlarmError::AlarmTooSoon)
+        let now = self.now_instant();
+        use fugit::MicrosDurationU32;
+        match self
+            .alarm
+            .borrow(cs)
+            .borrow_mut()
+            .deref_mut()
+            .as_mut()
+            .unwrap()
+            .schedule_at(min_timestamp)
             {
-                // TODO this is just a workaround - schedule at smallest possible interval
-                error!("delay increased to 10µs");
-                self
-                    .alarm
-                    .borrow(cs)
-                    .borrow_mut()
-                    .deref_mut()
-                    .as_mut()
-                    .unwrap()
-                    .schedule(MicrosDurationU32::micros(10)).unwrap();
-            } else {
-                trace!("alarm armed for {}", min_timestamp - now);
+                Result::Err(ScheduleAlarmError::AlarmTooSoon)
+                    => {
+                        // If alarm timestamp has passed, trigger it instantly.
+                        // This disarms it.
+                        trace!("timestamp has passed, trigger now! timestamp {} <= now {}", min_timestamp.ticks(), now.ticks());
+                        self.trigger_alarm(n, cs);
+                    },
+                Result::Err(ScheduleAlarmError::AlarmTooLate)
+                    => {
+                        // Duration >72 minutes. Reschedule 1 hour later.
+                        trace!("reschedule in 1h");
+                        self
+                            .alarm
+                            .borrow(cs)
+                            .borrow_mut()
+                            .deref_mut()
+                            .as_mut()
+                            .unwrap()
+                            .schedule(MicrosDurationU32::hours(1)).unwrap();
+                    },
+                Ok(_) => {
+                    trace!("min: {}, now: {}", min_timestamp.ticks(), now.ticks());
+                    trace!("alarm armed for {}", (min_timestamp - now).ticks());
+                },
+                Err(_) => core::panic!("Unknown error scheduling an alarm"),
             }
-        }
-
-        // If alarm timestamp has passed, trigger it instantly.
-        // This disarms it.
-        let now = self.now();
-        if min_timestamp <= now {
-            warn!("timestamp has passed, trigger now! timestamp {} <= now {}", min_timestamp, now);
-            self.trigger_alarm(n, cs);
-        }
     }
 
     fn check_alarm(&self) {
@@ -159,12 +172,12 @@ impl TimerDriver {
                 .unwrap()
                 .clear_interrupt();
             let next = self.next_scheduled_alarm();
-            if next.1 == u64::MAX {
+            if next.1 == NO_ALARM {
                 warn!("No next alarm. Spurious interrupt?");
                 return;
             }
             let (n, timestamp) = next;
-            let now = self.now();
+            let now = self.now_instant();
             // alarm peripheral has only 32 bits, so might have triggered early
             if timestamp <= now {
                 self.trigger_alarm(n, cs)
@@ -180,7 +193,7 @@ impl TimerDriver {
         // ignore... for now unsafe { pac::TIMER.armed().write(|w| w.set_armed(1 << n)) }
 
         let alarm = &self.alarms.borrow(cs)[n];
-        alarm.timestamp.set(u64::MAX);
+        alarm.timestamp.set(NO_ALARM);
 
         // Call after clearing alarm, so the callback can set another alarm.
         if let Some((f, ctx)) = alarm.callback.get() {
@@ -190,14 +203,14 @@ impl TimerDriver {
         trace!("callback executed");
     }
 
-    fn next_scheduled_alarm(&self) -> (usize, u64) {
+    fn next_scheduled_alarm(&self) -> (usize, Instant) {
         critical_section::with(|cs| {
             self.alarms
                 .borrow(cs)
                 .iter()
                 .map(|a| a.timestamp.get())
                 .enumerate()
-                .min_by_key(|p| p.1)
+                .min_by_key(|p| p.1.ticks())
                 .unwrap()
         })
     }
@@ -220,24 +233,10 @@ pub unsafe fn init(mut timer: Timer) {
         DRIVER.timer.borrow(cs).replace(Some(timer));
         let alarms = DRIVER.alarms.borrow(cs);
         for a in alarms {
-            a.timestamp.set(u64::MAX);
+            a.timestamp.set(NO_ALARM);
         }
     });
 
-    /*
-    // enable all irqs
-    pac::TIMER.inte().write(|w| {
-        w.set_alarm(0, true);
-        w.set_alarm(1, true);
-        w.set_alarm(2, true);
-        w.set_alarm(3, true);
-    });
-
-    interrupt::TIMER_IRQ_0::steal().enable();
-    interrupt::TIMER_IRQ_1::steal().enable();
-    interrupt::TIMER_IRQ_2::steal().enable();
-    interrupt::TIMER_IRQ_3::steal().enable();
-    */
 }
 
 #[interrupt]
@@ -246,19 +245,3 @@ unsafe fn TIMER_IRQ_0() {
     DRIVER.check_alarm()
 }
 
-/*
-#[interrupt]
-unsafe fn TIMER_IRQ_1() {
-    DRIVER.check_alarm(1)
-}
-
-#[interrupt]
-unsafe fn TIMER_IRQ_2() {
-    DRIVER.check_alarm(2)
-}
-
-#[interrupt]
-unsafe fn TIMER_IRQ_3() {
-    DRIVER.check_alarm(3)
-}
-*/
