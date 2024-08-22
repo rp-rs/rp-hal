ab006c3ecd0f7626e017ad699fd15aa8ab526829
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2024-08-22 19:53:28 +0100

    Update rp-hal-common/src/lib.rs
    Co-authored-by: 9names <60134748+9names@users.noreply.github.com>

--- rp-hal-common/src/lib.rs
+++ rp-hal-common/src/lib.rs
@@ -1,7 +1,7 @@
 //! Common HAL code
 //!
 //! This library contains types and functions which are shared between the
-//! RP2040 HAl and the RP235x HAL.
+//! RP2040 HAL and the RP235x HAL.
 //!
 //! You shouldn't include anything here which requires either the `cortex-m`
 //! crate, or a PAC.
