fa58345e7a2b73f1f136699f1aeb605e47fae750
Jan Niehusmann <jan@gondor.com>
2025-02-03 17:46:22 +0000

    Fix building of rp235x on-target tests

---
+++ on-target-tests/run_tests_rp235x.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Keep running tests even if one of them fails
+# We need to specify probe-rs as our runner via environment variables here
+# to control build since we aren't able to override them in config.toml
+CARGO_TARGET_THUMBV8M_MAIN_NONE_EABIHF_RUNNER="probe-rs run" cargo test --test dma_dyn --target thumbv8m.main-none-eabihf --no-fail-fast --features rp235x -- --chip rp235x
